375fa49b5607a20256dc839b3860b66e5f44e17d
Iuri Madeira <iurimadeira@gmail.com>
2023-07-03 11:01:21 -0300

    Use iurimadeira's fork of prometheus_ex

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule PrometheusEcto.Mixfile do
 
   defp deps do
     [
-      {:prometheus_ex, "~> 1.1 or ~> 2.0 or ~> 3.0"},
+      {:prometheus_ex, git: "https://github.com/iurimadeira/prometheus.ex", override: true},
       {:ecto, "~> 2.0 or ~> 3.0"},
       {:mariaex, ">= 0.0.0", only: :test},
       {:ex_doc, ">= 0.0.0", only: :dev},
